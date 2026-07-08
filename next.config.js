2f51292de2cb9065364bf55e20576b545e791d6b
michellrq25 <michellramirezq25@gmail.com>
2026-07-07 23:19:17 -0500

    clean: remove decap cms and oauth api routes

--- next.config.js
+++ next.config.js
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: '/admin',
-        destination: '/admin/index.html',
-        permanent: true,
-      },
-    ];
-  },
 };
 
 export default nextConfig;
